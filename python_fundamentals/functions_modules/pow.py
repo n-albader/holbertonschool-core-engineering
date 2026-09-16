c92a6301b69b6bcafc6ca0bdd97749cb80eceaca
n-albader <noor.abdullah.albader@gmail.com>
2026-09-16 19:39:40 +0300

    fix the negative

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 def pow(a, b):
     result = 1
+
+    if b < 0:
+        for i in range(-b):
+            result *= a
+        return 1 / result
+
     for i in range(b):
         result *= a
+
     return result
