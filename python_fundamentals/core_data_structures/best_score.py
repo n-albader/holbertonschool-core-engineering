6a43cd300e6b007f07dd73aa927fbac61cf2d8c1
n-albader <noor.abdullah.albader@gmail.com>
2026-09-19 16:38:34 +0300

    Add best_score function

---
+++ python_fundamentals/core_data_structures/best_score.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+def best_score(a_dictionary):
+    if a_dictionary is None or not a_dictionary:
+        return None
+
+    best_key = None
+    best_value = None
+
+    for key in a_dictionary:
+        if best_value is None or a_dictionary[key] > best_value:
+            best_value = a_dictionary[key]
+            best_key = key
+
+    return best_key
