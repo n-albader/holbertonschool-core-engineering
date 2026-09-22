a2f03914c2d837ea1d3dac7cc04efd7e9c045bce
n-albader <noor.abdullah.albader@gmail.com>
2026-09-22 12:45:14 +0300

    fix

--- python_oop/classes_and_object_model/6-square.py
+++ python_oop/classes_and_object_model/6-square.py
@@ -50,7 +50,7 @@ class Square:
                 "position must be a tuple of 2 positive integers"
             )
         if value[0] < 0 or value[1] < 0:
-           raise TypeError(
+            raise TypeError(
                 "position must be a tuple of 2 positive integers"
             )
         self.__position = value
