978111edca05fe7e1592b17af14b0b1faf232181
n-albader <noor.abdullah.albader@gmail.com>
2026-09-22 12:26:52 +0300

    fix Pycodestyle

--- python_oop/classes_and_object_model/6-square.py
+++ python_oop/classes_and_object_model/6-square.py
@@ -33,11 +33,23 @@ class Square:
     @position.setter
     def position(self, value):
         """Set the square position after validating it"""
-        if (not isinstance(value, tuple) or
-              len(value) != 2 or
-               not isinstance(value[0], int) or
-               not isinstance(value[1], int) or
-               value[0] < 0 or value[1] < 0):
+        if not isinstance(value, tuple):
+            raise TypeError(
+                "position must be a tuple of 2 positive integers"
+                )
+        if len(value) != 2:
+            raise TypeError(
+                "position must be a tuple of 2 positive integers"
+                )
+        if not isinstance(value[0], int):
+            raise TypeError(
+                "position must be a tuple of 2 positive integers"
+                )
+        if not isinstance(value[1], int):
+            raise TypeError(
+                "position must be a tuple of 2 positive integers"
+                )
+        if value[0] < 0 or value[1] < 0:
            raise TypeError(
                 "position must be a tuple of 2 positive integers"
                 )
