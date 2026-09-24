b44b904fc1239f39339941e1ec0fc2c12d4c4696
n-albader <noor.abdullah.albader@gmail.com>
2026-09-24 21:39:48 +0300

    Extend the Square class

---
+++ python_oop/inheritance/2-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""Defines the Square class"""
+
+
+Rectangle = __import__('2-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initialize a square with a size"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Return the area of the square"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Return the square description"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
