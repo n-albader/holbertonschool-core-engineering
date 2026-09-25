46bf18b81c187d3866c8295d993da18907491200
n-albader <noor.abdullah.albader@gmail.com>
2026-09-26 00:09:52 +0300

    add FlyingFish - Exploring Multiple Inheritance

---
+++ python_oop/abc/flyingfish.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+"""Defines Fish, Bird, and FlyingFish classes."""
+
+
+class Fish:
+    """Represents a fish."""
+
+    def swim(self):
+        """Make the fish swim."""
+        print("The fish is swimming")
+
+    def habitat(self):
+        """Describe the fish habitat."""
+        print("The fish lives in water")
+
+
+class Bird:
+    """Represents a bird."""
+
+    def fly(self):
+        """Make the bird fly."""
+        print("The bird is flying")
+
+    def habitat(self):
+        """Describe the bird habitat."""
+        print("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    """Represents a fish that can fly."""
+
+    def fly(self):
+        """Make the flying fish soar."""
+        print("The flying fish is soaring!")
+
+    def swim(self):
+        """Make the flying fish swim."""
+        print("The flying fish is swimming!")
+
+    def habitat(self):
+        """Describe the flying fish habitat."""
+        print("The flying fish lives both in water and the sky!")
